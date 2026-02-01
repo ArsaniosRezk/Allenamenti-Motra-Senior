c0bbe85a4d88a2462b7f4f520890789460368509
Arsanios Rezk <arsanios.rezk@gmail.com>
2026-02-01 10:29:33 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 // utils.js
 
-export const ID_SQUADRA = "motraA";
+export const ID_SQUADRA = "senior";
 
 export function mostraAvviso(messaggio, tipo = "success") {
     const avviso = document.getElementById("avviso");
